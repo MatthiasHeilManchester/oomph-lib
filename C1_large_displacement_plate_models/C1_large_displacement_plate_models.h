5e0790313ac96b81355bdca43a8987a092af5950
AidanRetallick <aidanretallick@outlook.com>
2023-06-20 19:17:57 +0100

    Reduce directory to just c1_basis and fvk src

--- C1_large_displacement_plate_models/C1_large_displacement_plate_models.h
+++
@@ -1,7 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include<C1_large_displacement_plate_models/large_displacement_plate_models.h> 
-#include<C1_large_displacement_plate_models/large_displacement_plate_elements.h> 
-#include<C1_large_displacement_plate_models/large_displacement_plate_curved_bell_elements.h> 
-#include<C1_large_displacement_plate_models/koiter_steigmann_plate_elements.h> 
-#include<C1_large_displacement_plate_models/foeppl_von_karman_correction_elements.h> 
